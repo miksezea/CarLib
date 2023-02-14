e21e5cfafc3e280c0eae24b4db1ab0939eaab4a1
mikke <mikke@AWPC>
2023-02-14 11:42:41 +0100

    Add project files.

---
+++ CarLib/Car.cs
@@ -0,0 +1,35 @@
+﻿namespace CarLib
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Model { get; set; }
+        public int Price { get; set; }
+        public string? LicensePlate { get; set; }
+        public void ValidateModel()
+        {
+            if (Model == null)
+                throw new ArgumentNullException("Model name can't be null");
+            if (Model.Length < 4)
+                throw new ArgumentException($"Model name can't be shorter than 4 letters. You typed: {nameof(Model)}");
+        }
+        public void ValidatePrice()
+        {
+            if (Price <= 0)
+                throw new ArgumentOutOfRangeException("Price cannot be 0 or less");
+        }
+        public void ValidateLicensePlate()
+        {
+            if (LicensePlate == null)
+                throw new ArgumentNullException("Licenseplate can't be null");
+            if (LicensePlate.Length < 2 || 7 < LicensePlate.Length)
+                throw new ArgumentException($"License should be between 2 and 7 characters. You typed: {nameof(LicensePlate)}");
+        }
+        public void ValidateCar()
+        {
+            ValidateModel();
+            ValidatePrice();
+            ValidateLicensePlate();
+        }
+    }
+}
---
+++ CarLibTests/CarTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLib.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car car = new() { Id = 1, Model = "test", Price = 1, LicensePlate = "AB"};
+        private Car car2 = new() { Id = 1, Model = "test", Price = 1, LicensePlate = "ABC1234" };
+        private Car carModelNull = new() { Id = 2, Model = null, Price = 50000};
+        private Car carModelLessThanFour = new() { Id = 3, Model = "hej", Price = 50000, LicensePlate = "ABC123" };
+        private Car carPriceLessThanOne = new() { Id = 4, Model = "test", Price = 0, LicensePlate = "ABC123" };
+        private Car carLicensePlateNull = new() { Id = 5, Model = "test", Price = 50000, LicensePlate = null };
+        private Car carLicensePlateLessThanTwo = new() { Id = 6, Model = "test", Price = 50000, LicensePlate = "A" };
+       
+        [TestMethod()]
+        public void ValidateModelExceptionTest()
+        {
+            car.ValidateModel();
+            Assert.ThrowsException<ArgumentNullException>(() => carModelNull.ValidateModel());
+            Assert.ThrowsException<ArgumentException>(() => carModelLessThanFour.ValidateModel());
+        }
+       
+        [TestMethod()]
+        public void ValidateModelPassingTest()
+        {
+            car.ValidateModel();
+            Assert.AreEqual("test", car.Model);
+        }
+        
+        [TestMethod()]
+        public void ValidatePriceExceptionTest()
+        {
+            car.ValidatePrice();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carPriceLessThanOne.ValidatePrice());
+        }
+        
+        [TestMethod()]
+        public void ValidatePricePassingTest()
+        {
+            car.ValidatePrice();
+            Assert.AreEqual(1, car.Price);
+        }
+
+        [TestMethod()]
+        public void ValidateLicensePlateExceptionTest()
+        {
+            car.ValidateLicensePlate();
+            Assert.ThrowsException<ArgumentNullException>(() => carLicensePlateNull.ValidateLicensePlate());
+            Assert.ThrowsException<ArgumentException>(() => carLicensePlateLessThanTwo.ValidateLicensePlate());
+        }
+
+        [TestMethod()]
+        public void ValidateLicensePlatePassingTest()
+        {
+            car.ValidateLicensePlate();
+
+            Assert.AreEqual("AB", car.LicensePlate);
+            Assert.AreEqual("ABC1234", car2.LicensePlate);
+        }
+
+        [TestMethod()]
+        public void ValidateCarTest()
+        {
+            car.ValidateCar();
+            Assert.ThrowsException<ArgumentException>(() => carModelLessThanFour.ValidateModel());
+        }
+    }
+}
