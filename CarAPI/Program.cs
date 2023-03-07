6f8fc1ac055288615f1d498b6e09f64744fa78fa
miksezea <mikk289e@edu.zealand.dk>
2023-03-07 10:39:44 +0100

    swagger

--- CarAPI/Program.cs
+++ CarAPI/Program.cs
@@ -3,13 +3,25 @@ using CarAPI.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll",
+                              policy =>
+                              {
+                                  policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                              });
+});
 builder.Services.AddControllers();
 builder.Services.AddSingleton<CarsRepository>(new CarsRepository());
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
